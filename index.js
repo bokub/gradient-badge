8bea7d415c574d6ad24a0457a02a159e03ff02c8
Boris Kubiak <kubiakboris@gmail.com>
2018-08-28 08:39:18 +0200

    Improve performance

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const badgen = require('badgen');
 const colorPresets = require('badgen/lib/color-presets.js');
 
 const addGradient = (str, grad) => {
-	let replacement = '  <linearGradient id="x" x1="0%" y1="0%" x2="100%" y2="0%">';
+	let svgGradient = '  <linearGradient id="x" x1="0%" y1="0%" x2="100%" y2="0%">';
 	for (let i = 0; i < grad.length; i++) {
 		const offset = Math.round((100 * i) / (grad.length - 1));
 		const color = colorPresets[grad[i]] || grad[i];
-		replacement += `\n          <stop offset="${offset}%" style="stop-color:#${color}" />`;
+		svgGradient += `\n    <stop offset="${offset}%" style="stop-color:#${color}" />`;
 	}
-	replacement += `\n        </linearGradient>\n      </svg>`;
-	return str.replace('</svg>', replacement);
+	svgGradient += `\n  </linearGradient>\n</svg>`;
+
+	const index = str.length - 6;
+
+	return str[index] === '<' ?
+		str.substr(0, index) + svgGradient : // Most efficient way
+		str.replace('</svg>', svgGradient); // Fallback in case of breaking changes
 };
 
 module.exports = function (options) {
