53203c6005f3ddb604eeb74ed82203413a169fa3
Boris Kubiak <kubiakboris@gmail.com>
2018-08-28 13:13:59 +0200

    Add tests, travis job and code coverage

---
+++ test.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import isSvg from 'is-svg';
+import colorPresets from 'badgen/lib/color-presets';
+import badgen from 'badgen';
+import badges from '.';
+
+for (const style of ['classic', 'flat']) {
+	const prefix = style === 'classic' ? '[classic] ' : '   [flat] ';
+	const params = {subject: 'version', status: 'v1.2.3', style, gradient: ['pink', 'F78642']};
+	const original = badgen(params);
+	const badge = badges(params);
+
+	console.log(badge);
+	test(prefix + 'is svg', t => {
+		t.true(isSvg(badge));
+	});
+
+	test(prefix + 'has the good colors', t => {
+		t.true(badge.indexOf('F78642') > -1);
+		t.true(badge.indexOf(colorPresets.pink) > -1);
+	});
+
+	test(prefix + 'uses the linear gradient', t => {
+		t.true(badge.indexOf('url(#x)') > -1);
+		t.true(badge.indexOf('id="x"') > -1);
+	});
+
+	test(prefix + 'badgen has no breaking changes', t => {
+		t.is(original.indexOf('</svg>'), original.length - 6);
+	});
+}
+
